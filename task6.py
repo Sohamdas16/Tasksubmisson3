e68e916b74ff206d957f1a8443e114fa781af346
Sohamdas16 <sohamd824@gmail.com>
2026-01-08 22:42:15 +0530

    Create task6.py

---
+++ task6.py
@@ -0,0 +1,10 @@
+import math
+n=int(input("Enter a number"))
+s=math.sqrt(n)
+if n>0:
+    b=math.log(n)
+r=math.radians(n)
+c=math.sin(r)
+print("The square root is",s)
+print("The log of the number is",b)
+print( "Sine :",c)
